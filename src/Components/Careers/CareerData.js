bc8c41e78756bd4e53db6b80706814e21a0c7d41
raheel-eshaafi <raheel@eshaafi.com>
2022-01-19 12:12:08 +0500

    careers_hald_done

---
+++ src/Components/Careers/CareerData.js
@@ -0,0 +1,45 @@
+export const CareerData = [
+  {
+    text: "Junior Unity Developer",
+    descL:
+      "Looking for a fresh graduate with passion for game development, strong OOP and data structure concepts required.",
+    btn: "Apply Now",
+  },
+  {
+    text: "Post Production CG Artist",
+    descL:
+      "Looking for 2-3 years experienced CG Artist for post-production work on game screenshots and icons.",
+    btn: "Apply Now",
+  },
+  {
+    text: "Concept Artist",
+    descL:
+      "Looking for 2-3 years experienced character and environment artist for creating sketches and storyboarding.",
+    btn: "Apply Now",
+  },
+  {
+    text: "3D Modeler",
+    descL:
+      "Looking for 3-4 years experienced character and environment modeler with expertise in Blender and Maya.",
+    btn: "Apply Now",
+  },
+  {
+    text: "3D Animator",
+    descL:
+      "Looking for 3-4 years experienced character animator who has worked in gaming industry and can bring characters to life.",
+    btn: "Apply Now",
+  },
+  {
+    text: "Art Director",
+    descL:
+      "Looking for 6-8 years experienced Art Director to  guide the art style of the game. Strong academic background with hands-on work is must.",
+    btn: "Apply Now",
+  },
+
+  {
+    text: "HR Officer",
+    descL:
+      "Looking for fresh MBA/BBA HR graduate, with strong communication skills and pleasant personality, able to multitask effectively.",
+    btn: "Apply Now",
+  },
+];
--- src/Pages/Careers.js
+++ src/Pages/Careers.js
@@ -12,6 +12,23 @@ import "react-multi-carousel/lib/styles.css";
 import Waqar from "../Resources/Images/career/waqar.svg";
 
 import kh from "../Resources/Images/career/kh.svg";
+
+import career_culture_first from "../Resources/Images/career/career_culture_first.png";
+
+import Heart_icon from "../Resources/Images/career/Heart_icon.svg";
+
+import attandance from "../Resources/Images/career/attandance.svg";
+
+import paid_leave from "../Resources/Images/career/paid_leave.svg";
+
+import loyality from "../Resources/Images/career/loyality.svg";
+
+import provident from "../Resources/Images/career/provident.svg";
+
+import paid from "../Resources/Images/career/paid.svg";
+
+import { CareerData } from "../Components/Careers/CareerData.js";
+
 function Careers() {
   const responsive = {
     superLargeDesktop: {
@@ -170,6 +187,163 @@ function Careers() {
                 </div>
               </div>
             </Col>
+          </Row>
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <Row className="career_culture_wrapper">
+            <Col
+              lg={6}
+              className="d-flex justify-content-center align-items-center"
+            >
+              <div className="col-lg-9">
+                <div className="career_our_culture_heading">
+                  <h6>our culture</h6>
+                </div>
+                <div className="career_our_culture_text">
+                  <p>
+                    We believe in building a culture where employee engagement,
+                    loyalty, learning, and dedication is encouraged. We strive
+                    to empower our employees to step out of their comfort zones
+                    and embrace new challenges for both personal and
+                    professional growth.{" "}
+                  </p>
+                </div>
+              </div>
+            </Col>
+            <Col lg={6}>
+              <div>
+                <img src={career_culture_first} alt="...." />
+              </div>
+            </Col>
+          </Row>
+
+          <Row className="career_culture_wrapper">
+            <Col lg={6}>
+              <div>
+                <img src={career_culture_first} alt="...." />
+              </div>
+            </Col>
+            <Col
+              lg={6}
+              className="d-flex justify-content-center align-items-center"
+            >
+              <div className="col-lg-9">
+                <div className="career_our_culture_heading">
+                  <h6>our culture</h6>
+                </div>
+                <div className="career_our_culture_text">
+                  <p>
+                    We believe in building a culture where employee engagement,
+                    loyalty, learning, and dedication is encouraged. We strive
+                    to empower our employees to step out of their comfort zones
+                    and embrace new challenges for both personal and
+                    professional growth.
+                  </p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="text-center career_employee_benefite">
+                <div className="Employee_Benifits_Careeer">
+                  <h4>Employee Benifits</h4>
+                </div>
+
+                <div>
+                  <h5>
+                    Aurelian Origins is focused on providing various benefits
+                    for employee wellbeing.
+                  </h5>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className="employee_benefits_wrapper">
+            <Col lg={12} className="d-flex justify-content-center">
+              <div className="col-lg-12 d-flex justify-content-between">
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={Heart_icon} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Employee Wellbeing</p>
+                  </div>
+                </div>
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={attandance} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Attendance Rewards</p>
+                  </div>
+                </div>
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={paid_leave} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Paid Leaves</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className="employee_career_well_beign_second">
+            <Col lg={12} className="d-flex justify-content-center">
+              <div className="col-lg-12 d-flex justify-content-between">
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={loyality} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Loyalty Rewards</p>
+                  </div>
+                </div>
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={provident} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Provident Fund</p>
+                  </div>
+                </div>
+                <div className="col-lg-4 text-center">
+                  <div>
+                    <img src={paid} alt="..."></img>
+                  </div>
+                  <div className="craeer_employee_wellBeing">
+                    <p>Paid Overtime</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="career_open_position_wrapper">
+                <h4>Open positions</h4>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            {CareerData.map((data, i) => (
+              <Col lg={4} className="job_openings_wrapper" key={i}>
+                <div className="text-center">
+                  <div className="career_position_head text-center">
+                    <h4>{data.text}</h4>
+                  </div>
+                  <div className="career_position_para text-center col-lg-12">
+                    <p>{data.descL}</p>
+                  </div>
+                  <div className="career_position_btn">
+                    <button>{data.btn}</button>
+                  </div>
+                </div>
+              </Col>
+            ))}
           </Row>
         </Container>
       </div>
